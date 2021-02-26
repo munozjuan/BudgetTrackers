924ce5428d2926de9a3fda9e49aa0b59084bfd24
munozjuan <munoz.juan07@hotmail.com>
2021-02-26 18:05:55 -0500

    added con.log to cache

--- models/service.js
+++ models/service.js
@@ -3,3 +3,15 @@ const FILES_TO_CACHE = [
 
     const CACHE_NAME = "static-cache-v2";
     const DATA_CACHE_NAME = "data-cache-v1";
+
+  // install function
+  self.addEventListener("install", function(evt){
+      evt.waitUntil(
+          caches.open(CACHE_NAME).then(cache =>{
+              console.log("Files were pre-cached!");
+              return cache.addAll(FILES_TO_CACHE);
+          })
+      );
+
+      self.skipWaiting();
+    });
